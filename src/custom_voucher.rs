ad921df9b05054f1941d483fa809f4b7d9c8bb22
j-devel <j@w3reality.com>
2022-02-28 18:21:56 +0700

    Refactor mbedtls backends into `mod {rust,minerva}_mbedtls_backend`

--- src/custom_voucher.rs
+++ src/custom_voucher.rs
@@ -31,20 +31,9 @@ impl CustomVoucher {
     }
 }
 
-//
-
-type CustomError = mbedtls::Error;
-const ERROR_ASN1_FAILED: i32 = -1;
-
-use mbedtls::pk::{EcGroup, EcGroupId, Pk, ECDSA_MAX_LEN};
-use mbedtls::ecp::EcPoint;
-use mbedtls::x509::certificate::Certificate;
-use mbedtls::hash as mbedtls_hash;
-use crate::support_rand::test_rng;
-
 impl Sign for CustomVoucher {
     fn sign(&mut self, privkey_pem: &[u8], alg: SignatureAlgorithm) -> Result<&mut Self, VoucherError> {
-        if let Err(_) = sign_with_rust_mbedtls(privkey_pem, alg, self.to_sign(alg)) {
+        if let Err(_) = backend::sign(privkey_pem, alg, self.to_sign(alg)) {
             Err(VoucherError::SigningFailed)
         } else {
             Ok(self)
@@ -52,27 +41,9 @@ impl Sign for CustomVoucher {
     }
 }
 
-fn sign_with_rust_mbedtls(
-    privkey_pem: &[u8],
-    alg: SignatureAlgorithm,
-    (sig_out, sig_struct): (&mut Vec<u8>, &[u8])
-) -> Result<(), CustomError> {
-    let (ref hash, md_ty) = compute_digest(sig_struct, &alg)?;
-    let mut sig = vec![0u8; ECDSA_MAX_LEN];
-    let sig_len = Pk::from_private_key(&utils::null_terminate_bytes!(privkey_pem), None)?
-        .sign_deterministic(md_ty, &hash, &mut sig, &mut test_rng())?;
-    sig.truncate(sig_len);
-
-    *sig_out = sig;
-
-    Ok(())
-}
-
-//
-
 impl Validate for CustomVoucher {
     fn validate(&self, pem: Option<&[u8]>) -> Result<&Self, VoucherError> {
-        match validate_with_rust_mbedtls(pem, self.to_validate()) {
+        match backend::validate(pem, self.to_validate()) {
             Ok(true) => Ok(self),
             Ok(false) => Err(VoucherError::ValidationFailed),
             Err(_) => Err(VoucherError::ValidationFailed),
@@ -80,64 +51,108 @@ impl Validate for CustomVoucher {
     }
 }
 
-fn validate_with_rust_mbedtls(
-    pem: Option<&[u8]>,
-    (signer_cert, sig_alg, msg): (Option<&[u8]>, Option<(&[u8], &SignatureAlgorithm)>, &[u8])
-) -> Result<bool, CustomError> {
-    if sig_alg.is_none() { return Ok(false); }
-    let (signature, alg) = sig_alg.unwrap();
-
-    let ref signature = if utils::is_asn1_signature(signature) {
-        signature.to_vec()
-    } else {
-        utils::asn1_signature_from(signature).or(Err(CustomError::Other(ERROR_ASN1_FAILED)))?
-    };
-
-    let (ref hash, md_ty) = compute_digest(msg, alg)?;
-
-    if let Some(pem) = pem {
-        let pem = &utils::null_terminate_bytes!(pem);
-
-        if let Ok(mut pk) = Pk::from_private_key(pem, None) {
-            return pk.verify(md_ty, hash, signature).and(Ok(true));
-        }
-
-        Certificate::from_pem(pem)?
-            .public_key_mut()
-            .verify(md_ty, hash, signature)
-            .and(Ok(true))
-    } else if let Some(cert) = signer_cert {
-        let grp = EcGroup::new(EcGroupId::SecP256R1)?;
-
-        let prefix = *cert.get(0).unwrap();
-        if prefix == 0x02 || prefix == 0x03 {
-            // "Compressed point, which mbedtls does not understand"
-            // according to 'src/ecp/mod.rs' of the `rust-mbedtls` crate
-            println!("validate(): warning: `cert` cannot be processed by vanilla mbedtls");
-        }
-        let pt = EcPoint::from_binary(&grp, cert)?;
-
-        Pk::public_from_ec_components(grp.clone(), pt)?
-            .verify(md_ty, hash, signature)
-            .and(Ok(true))
-    } else {
-        println!("validate(): Neither external masa cert nor signer cert is available.");
-        Ok(false)
-    }
-}
-
 //
 
-fn compute_digest(msg: &[u8], alg: &SignatureAlgorithm) -> Result<(Vec<u8>, mbedtls_hash::Type), CustomError> {
-    let (md_type, digest_len) = match *alg {
-        SignatureAlgorithm::ES256 => (mbedtls_hash::Type::Sha256, 32),
-        SignatureAlgorithm::ES384 => (mbedtls_hash::Type::Sha384, 48),
-        SignatureAlgorithm::ES512 => (mbedtls_hash::Type::Sha512, 64),
-        SignatureAlgorithm::PS256 => unimplemented!("handle PS256"),
-    };
+#[cfg(feature = "minerva-mbedtls")]
+use minerva_mbedtls_backend as backend;
+#[cfg(feature = "mbedtls")]
+use rust_mbedtls_backend as backend;
 
-    let mut digest = vec![0u8; digest_len];
-    mbedtls_hash::Md::hash(md_type, msg, &mut digest)?;
+#[cfg(feature = "minerva-mbedtls")]
+mod minerva_mbedtls_backend {
+    use super::*;
 
-    Ok((digest, md_type))
+    // WIP - port
+    //  https://github.com/AnimaGUS-minerva/voucher/blob/master/src/sign.rs
+    //  https://github.com/AnimaGUS-minerva/voucher/blob/master/src/validate.rs
+}
+
+#[cfg(feature = "mbedtls")]
+mod rust_mbedtls_backend {
+    use super::*;
+
+    type CustomError = mbedtls::Error;
+    const ERROR_ASN1_FAILED: i32 = -1;
+
+    use mbedtls::pk::{EcGroup, EcGroupId, Pk, ECDSA_MAX_LEN};
+    use mbedtls::ecp::EcPoint;
+    use mbedtls::x509::certificate::Certificate;
+    use mbedtls::hash as mbedtls_hash;
+    use crate::support_rand::test_rng;
+
+    pub fn sign(
+        privkey_pem: &[u8],
+        alg: SignatureAlgorithm,
+        (sig_out, sig_struct): (&mut Vec<u8>, &[u8])
+    ) -> Result<(), CustomError> {
+        let (ref hash, md_ty) = compute_digest(sig_struct, &alg)?;
+        let mut sig = vec![0u8; ECDSA_MAX_LEN];
+        let sig_len = Pk::from_private_key(&utils::null_terminate_bytes!(privkey_pem), None)?
+            .sign_deterministic(md_ty, &hash, &mut sig, &mut test_rng())?;
+        sig.truncate(sig_len);
+
+        *sig_out = sig;
+
+        Ok(())
+    }
+
+    pub fn validate(
+        pem: Option<&[u8]>,
+        (signer_cert, sig_alg, msg): (Option<&[u8]>, Option<(&[u8], &SignatureAlgorithm)>, &[u8])
+    ) -> Result<bool, CustomError> {
+        if sig_alg.is_none() { return Ok(false); }
+        let (signature, alg) = sig_alg.unwrap();
+
+        let ref signature = if utils::is_asn1_signature(signature) {
+            signature.to_vec()
+        } else {
+            utils::asn1_signature_from(signature).or(Err(CustomError::Other(ERROR_ASN1_FAILED)))?
+        };
+
+        let (ref hash, md_ty) = compute_digest(msg, alg)?;
+
+        if let Some(pem) = pem {
+            let pem = &utils::null_terminate_bytes!(pem);
+
+            if let Ok(mut pk) = Pk::from_private_key(pem, None) {
+                return pk.verify(md_ty, hash, signature).and(Ok(true));
+            }
+
+            Certificate::from_pem(pem)?
+                .public_key_mut()
+                .verify(md_ty, hash, signature)
+                .and(Ok(true))
+        } else if let Some(cert) = signer_cert {
+            let grp = EcGroup::new(EcGroupId::SecP256R1)?;
+
+            let prefix = *cert.get(0).unwrap();
+            if prefix == 0x02 || prefix == 0x03 {
+                // "Compressed point, which mbedtls does not understand"
+                // according to 'src/ecp/mod.rs' of the `rust-mbedtls` crate
+                println!("validate(): warning: `cert` cannot be processed by vanilla mbedtls");
+            }
+            let pt = EcPoint::from_binary(&grp, cert)?;
+
+            Pk::public_from_ec_components(grp.clone(), pt)?
+                .verify(md_ty, hash, signature)
+                .and(Ok(true))
+        } else {
+            println!("validate(): Neither external masa cert nor signer cert is available.");
+            Ok(false)
+        }
+    }
+
+    fn compute_digest(msg: &[u8], alg: &SignatureAlgorithm) -> Result<(Vec<u8>, mbedtls_hash::Type), CustomError> {
+        let (md_type, digest_len) = match *alg {
+            SignatureAlgorithm::ES256 => (mbedtls_hash::Type::Sha256, 32),
+            SignatureAlgorithm::ES384 => (mbedtls_hash::Type::Sha384, 48),
+            SignatureAlgorithm::ES512 => (mbedtls_hash::Type::Sha512, 64),
+            SignatureAlgorithm::PS256 => unimplemented!("handle PS256"),
+        };
+
+        let mut digest = vec![0u8; digest_len];
+        mbedtls_hash::Md::hash(md_type, msg, &mut digest)?;
+
+        Ok((digest, md_type))
+    }
 }
